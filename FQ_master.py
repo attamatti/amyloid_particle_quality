9d101820f42778f451e979f62cf249cbd6e308ab
Matthew Iadanza <fbsmi@fbsabsl-wkstn06.leeds.ac.uk>
2019-05-15 12:44:17 +0100

    more verbose output

--- FQ_master.py
+++ FQ_master.py
@@ -14,7 +14,10 @@ except:
 
 # hello world
 print('{0} files'.format(len(files)))
+filecount = 0
 for i in files:
+    filecount+=1
+    print('\nworking on file {0}/{1}'.format(filecount,len(files)))
     # unstack the file
     print('\nunstacking {0}'.format(i))
     workingdir = '/'.join(i.split('/')[0:-1])
@@ -22,21 +25,23 @@ for i in files:
     print('working directory: {0}/'.format(workingdir))
     unstackedfiles = glob.glob('{0}/*.mrc'.format(workingdir))
     print('analysing particles')
+    # run the fibril quality script
     subprocess.call(['./fibril_quality.py','--i','{0}/'.format(workingdir),'--apix','1.065','--noimages'])
     print ('\ncleaning up {0} unstacked files'.format(len(unstackedfiles)))
     n=0
+    # clean up   
     for i in unstackedfiles:
         subprocess.call(['rm',i])
         n+=1
         if n%10 == 0:
             sys.stdout.write('.')
             sys.stdout.flush()
-
+    
 # make the final histogram
 nums = []
 for i in open('FQ_find.log','r').readlines():
     nums.append(float(i.split()[-1]))
-print('{0} total particles analysed'.format(len(nums)))
+print('\n{0} total particles analysed'.format(len(nums)))
 plt.hist(nums)
 plt.savefig('FQ_histo.png')
     
